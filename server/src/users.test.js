c8c6d9bf83129a58395886da809754f3a3bec013
polydome <polydome@outlook.com>
2021-10-13 19:17:03 +0200

    Create POST /users tests

---
+++ server/src/users.test.js
@@ -0,0 +1,70 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('Create user', () => {
+    describe('when no request body', () => {
+        it('should respond with 400', async () => {
+            const res = await request(app)
+                .post('/api/users');
+
+            expect(res.status).toBe(400);
+        })
+    });
+
+    describe('when password missing in request body', () => {
+        it('should respond with 400', async () => {
+            const res = await request(app)
+                .post('/api/users')
+                .send(MISSING_PASSWORD_BODY);
+
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('when username missing in request body', () => {
+        it('should respond with 400', async () => {
+            const res = await request(app)
+                .post('/api/users')
+                .send(MISSING_USERNAME_BODY);
+
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('when registered successfuly', () => {
+        it('should respond with 200', async () => {
+            const res = await request(app)
+                .post('/api/users')
+                .send(VALID_BODY)
+
+            expect(res.status).toBe(200);
+        });
+    });
+
+    describe('when registering twice with a same name', () => {
+        it('should respond with 409', async () => {
+            await request(app)
+                .post('/api/users')
+                .send(VALID_BODY);
+
+            const res = await request(app)
+                .post('/api/users')
+                .send(VALID_BODY);
+
+            expect(res.status).toBe(409);
+        });
+    });
+
+    const VALID_BODY = {
+        'username': 'testuser',
+        'password': 'testpass'
+    }
+
+    const MISSING_PASSWORD_BODY = {
+        'username': 'testuser'
+    };
+
+    const MISSING_USERNAME_BODY = {
+        'password': 'testpass'
+    }
+})
