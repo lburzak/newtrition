6535f5683b1fd0fa22ae8c2e7487a968b8e237ec
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-11-04 16:24:00 +0100

    Create app page layout

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {List, ListItem, ListItemIcon} from "@material-ui/core";
+import Box from '@mui/material/Box';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import {Restaurant, Fastfood} from '@mui/icons-material';
+import {Divider} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-        <p>Newtrition - under construction</p>
+        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            <List>
+                <ListItem>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Fastfood />
+                        </ListItemIcon>
+                        <ListItemText primary="Products" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <Restaurant />
+                        </ListItemIcon>
+                        <ListItemText primary="Recipes" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+        <Box>
+            <Divider orientation={"vertical"} variant={"middle"}/>
+        </Box>
     </div>
   );
 }
