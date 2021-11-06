cc425fa5c701c32e0bb05ed16ebe5af6015573a2
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-11-06 21:54:11 +0100

    Replace custom username/password validator with Joi

--- server/src/models/validationError.js
+++ server/src/models/validationError.js
@@ -9,8 +9,5 @@ class ValidationError {
 }
 
 module.exports = {
-    tooLong: (field) => new ValidationError(field, "Too long."),
-    tooShort: (field) => new ValidationError(field, "Too short."),
-    containsIllegalCharacters: (field) => new ValidationError(field, "Contains illegal characters."),
     fromJoiError: (validationError) => new ValidationError(validationError.details[0].path[0], validationError.message)
 }
--- server/src/services/validationService.js
+++ server/src/services/validationService.js
@@ -1,34 +1,13 @@
-const ValidationError = require('../models/validationError');
 const Joi = require("joi");
 
-function validateUsername(username) {
-    const field = 'username';
-    const errors = [];
+const usernameSchema = Joi.string()
+    .min(2)
+    .max(40)
+    .pattern(/^([A-Za-z\-]+)$/);
 
-    if (username.length < 2)
-        errors.push(ValidationError.tooShort(field));
-
-    if (username.length > 40)
-        errors.push(ValidationError.tooLong(field));
-
-    if (!/^([A-Za-z\-]+)$/.test(username))
-        errors.push(ValidationError.containsIllegalCharacters(field));
-
-    return errors;
-}
-
-function validatePassword(password) {
-    const field = 'password';
-    const errors = [];
-
-    if (password.length < 5)
-        errors.push(ValidationError.tooShort(field));
-
-    if (password.length > 64)
-        errors.push(ValidationError.tooLong(field));
-
-    return errors;
-}
+const passwordSchema = Joi.string()
+    .min(5)
+    .max(64);
 
 const eanSchema = Joi.string()
     .length(13)
@@ -38,14 +17,10 @@ const productSchema = Joi.object({
     ean: eanSchema,
     name: Joi.string().required(),
     nutritionFacts: Joi.object()
-})
-
-function validateProduct(product) {
-    return productSchema.validate(product);
-}
+});
 
 module.exports = {
-    validateUsername,
-    validatePassword,
-    validateProduct
+    validateUsername: usernameSchema.validate,
+    validatePassword: passwordSchema.validate,
+    validateProduct: productSchema.validate
 }
