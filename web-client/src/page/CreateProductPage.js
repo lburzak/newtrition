e2a576c66512b17a5019ee918830a6963972615e
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-11-23 13:28:48 +0100

    Refactor new product sheet

--- web-client/src/page/CreateProductPage.js
+++ web-client/src/page/CreateProductPage.js
@@ -4,12 +4,9 @@ import {
     FormHelperText,
     Grid,
     InputAdornment,
-    Paper, TextField,
-    Typography,
-    OutlinedInput,
-    IconButton
+    TextField,
+    OutlinedInput
 } from "@mui/material";
-import {Close} from "@mui/icons-material";
 
 const DetailInput = ({name, unit}) => <FormControl variant="outlined">
     <FormHelperText>{name}</FormHelperText>
@@ -20,37 +17,28 @@ const DetailInput = ({name, unit}) => <FormControl variant="outlined">
     />
 </FormControl>
 
-// TODO: Move `onClose` to a container for show/hide logic
-export const CreateProductPage = ({onClose}) => {
-    return <Paper elevation={4} style={{minHeight: 400, width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', paddingLeft: 40, paddingRight: 40, paddingBottom: 40}}>
-        <div>
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Typography variant={'h6'} style={{margin: 20, color: 'hsl(0, 0%, 40%)', textAlign: 'start'}}>New product</Typography>
-                <IconButton onClick={onClose}>
-                    <Close style={{margin: 20, color: 'hsl(0, 0%, 40%)'}}/>
-                </IconButton>
-            </div>
-            <Grid container spacing={2} separator>
-                <Grid item xs={6} md={8}>
-                    <TextField fullWidth label={"Product name"} variant={"outlined"}/>
-                </Grid>
-                <Grid item xs={6} md={4}>
-                    <TextField fullWidth label={"EAN Code"} variant={"outlined"}/>
-                </Grid>
-                <Grid item xs={3}>
-                    <DetailInput name="Calories per 100g" unit="kcal"/>
-                </Grid>
-                <Grid item xs={3}>
-                    <DetailInput name="Proteins per 100g" unit="grams"/>
-                </Grid>
-                <Grid item xs={3}>
-                    <DetailInput name="Carbohydrates per 100g" unit="grams"/>
-                </Grid>
-                <Grid item xs={3}>
-                    <DetailInput name="Fat per 100g" unit="grams"/>
-                </Grid>
+export const CreateProductPage = () =>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%'}}>
+        <Grid container spacing={2} separator>
+            <Grid item xs={6} md={8}>
+                <TextField fullWidth label={"Product name"} variant={"outlined"}/>
             </Grid>
-        </div>
+            <Grid item xs={6} md={4}>
+                <TextField fullWidth label={"EAN Code"} variant={"outlined"}/>
+            </Grid>
+            <Grid item xs={3}>
+                <DetailInput name="Calories per 100g" unit="kcal"/>
+            </Grid>
+            <Grid item xs={3}>
+                <DetailInput name="Proteins per 100g" unit="grams"/>
+            </Grid>
+            <Grid item xs={3}>
+                <DetailInput name="Carbohydrates per 100g" unit="grams"/>
+            </Grid>
+            <Grid item xs={3}>
+                <DetailInput name="Fat per 100g" unit="grams"/>
+            </Grid>
+        </Grid>
+
         <Button fullWidth variant={"contained"} type={"submit"}>Save</Button>
-    </Paper>
-}
+    </div>
--- web-client/src/page/DashboardPage.js
+++ web-client/src/page/DashboardPage.js
@@ -1,7 +1,16 @@
 import Box from "@mui/material/Box";
-import {Button, Divider, Fab, List, ListItem, ListItemIcon} from "@mui/material";
+import {
+    Button,
+    Divider,
+    Fab,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon, Paper,
+    Typography
+} from "@mui/material";
 import ListItemButton from "@mui/material/ListItemButton";
-import {AccountCircle, Fastfood, Add, Restaurant} from "@mui/icons-material";
+import {AccountCircle, Fastfood, Add, Restaurant, Close} from "@mui/icons-material";
 import ListItemText from "@mui/material/ListItemText";
 import {AuthContext} from "../App";
 import {useContext, useEffect, useState} from "react";
@@ -21,6 +30,35 @@ const ProfileView = () => <AuthContext.Consumer>
         </ListItem>
     </Box>}
 </AuthContext.Consumer>
+
+const BottomSheet = ({title, visible, onDismiss, children}) =>
+    <div style={{visibility: visible ? 'visible' : 'hidden', zIndex: 2, display: 'flex', justifyContent: 'center'}}>
+        <Paper elevation={4} style={{
+            minHeight: 400,
+            width: '90%',
+            paddingLeft: 40,
+            paddingRight: 40,
+            paddingBottom: 40,
+            display: 'flex'
+        }}>
+            <div style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Typography variant={'h6'}
+                                style={{margin: 20, color: 'hsl(0, 0%, 40%)', textAlign: 'start'}}>{title}</Typography>
+                    <IconButton onClick={onDismiss}>
+                        <Close style={{margin: 20, color: 'hsl(0, 0%, 40%)'}}/>
+                    </IconButton>
+                </div>
+                <div style={{flex: 1}}>
+                    {children}
+                </div>
+            </div>
+        </Paper>
+    </div>
 
 export const DashboardPage = () => {
     const [productCreatorOpened, setProductCreatorOpened] = useState(false);
@@ -57,9 +95,10 @@ export const DashboardPage = () => {
         </Box>
         <Divider variant={'fullWidth'} orientation={'vertical'}/>
         <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', flex: 1}}>
-            <div style={{visibility: productCreatorOpened ? 'visible' : 'hidden', zIndex: 2}}>
-                <CreateProductPage onClose={() => setProductCreatorOpened(false)}/>
-            </div>
+            <BottomSheet title="New product" visible={productCreatorOpened}
+                         onDismiss={() => setProductCreatorOpened(false)}>
+                <CreateProductPage/>
+            </BottomSheet>
             <Fab color={'primary'} variant={"extended"} sx={{mr: 1}}
                  style={{position: 'absolute', margin: 20}} onClick={() => setProductCreatorOpened(true)}>
                 <Add/> New product
