40d68337bc7b21d720473210af0bdc49525a8a92
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-12-06 13:48:23 +0100

    Create placeholder products list

--- web-client/src/page/DashboardPage.js
+++ web-client/src/page/DashboardPage.js
@@ -1,8 +1,8 @@
 import Box from "@mui/material/Box";
 import {
-    Button,
+    Button, Card,
     Divider,
-    Fab,
+    Fab, Grid,
     IconButton,
     List,
     ListItem,
@@ -70,8 +70,8 @@ export const DashboardPage = () => {
             navigate('/login');
     }, [authState, navigate]);
 
-    return <div style={{display: 'flex', flexDirection: 'row', width: '100vw'}}>
-        <Box sx={{width: '100%', maxWidth: 360, height: '100vh', bgcolor: 'background.paper'}}
+    return <div style={{display: 'flex', flexDirection: 'row', width: '100vw', height: '100vh'}}>
+        <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
              style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
             <List>
                 <ListItem disablePadding>
@@ -94,15 +94,51 @@ export const DashboardPage = () => {
             <ProfileView/>
         </Box>
         <Divider variant={'fullWidth'} orientation={'vertical'}/>
-        <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', flex: 1}}>
+        <Box style={{overflowY: 'scroll', height: '100%'}}>
+            <div style={{padding: 20}}>
+                <EvenGrid>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942} imageSrc={'https://www.sadowniczy.pl/pol_pl_Kasza-Gryczana-PRAZONA-2-5kg-ciemna-160292_1.jpg'}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                    <ProductCard name={'Kasza gryczana'} calories={300} proteins={200} carbohydrates={100} ean={8271847273942}/>
+                </EvenGrid>
+            </div>
+        </Box>
+        <Box style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', flex: 1, position: 'absolute'}}>
             <BottomSheet title="New product" visible={productCreatorOpened}
                          onDismiss={() => setProductCreatorOpened(false)}>
                 <CreateProductPage/>
             </BottomSheet>
-            <Fab color={'primary'} variant={"extended"} sx={{mr: 1}}
-                 style={{position: 'absolute', margin: 20}} onClick={() => setProductCreatorOpened(true)}>
-                <Add/> New product
-            </Fab>
         </Box>
+        <Fab color={'primary'} variant={"extended"} sx={{mr: 1}}
+             style={{position: 'absolute', right: 20, bottom: 20}} onClick={() => setProductCreatorOpened(true)}>
+            <Add/> New product
+        </Fab>
     </div>;
 }
+
+const EvenGrid = ({children}) => <Grid container spacing={2}>
+    {children.map(child => <Grid item xs={12} sm={6} md={4} lg={3}>{child}</Grid>)}
+</Grid>
+
+const ProductSpecRow = ({name, value}) => <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+    <Typography variant={'body1'} style={{color: 'hsl(0, 0%, 70%)'}}>{name}</Typography>
+    <Typography variant={'body1'}>{value}</Typography>
+</div>
+
+const ProductCard = ({name, calories, proteins, carbohydrates, ean, imageSrc}) => <Card>
+    <div style={{height: 120, width: '100%', backgroundColor: 'gray'}}>
+        {imageSrc ? <img style={{objectFit: 'cover', width: '100%', height: '100%'}} src={imageSrc} alt={`${name}`}/> : <div/>}
+    </div>
+    <div style={{display: 'flex', flexDirection: 'column', padding: 10}}>
+        <Typography variant={'h6'} fontWeight={'bold'}>{name}</Typography>
+        <Typography variant={'body1'} fontFamily={'monospace'} style={{color: 'hsl(0, 0%, 50%)'}}>{ean}</Typography>
+        <Divider orientation={'horizontal'}/>
+        <ProductSpecRow name={'Calories'} value={calories}/>
+        <ProductSpecRow name={'Proteins'} value={proteins}/>
+        <ProductSpecRow name={'Carbs'} value={carbohydrates}/>
+    </div>
+</Card>
