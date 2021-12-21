0a04671a37337bf3e58b5c0b449b097b56198ab3
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2021-12-21 21:10:32 +0100

    Create utils for creating test client for integration tests

---
+++ server/src/util/testClient.js
@@ -0,0 +1,26 @@
+const https = require("https");
+const newtritionApp = require("../app");
+const NewtritionClient = require("js-client")
+const http = require("http");
+
+function getAddress(server, path) {
+    const address = server.address();
+
+    if (!address) server.listen(0);
+    const port = server.address().port;
+    const protocol = server instanceof https.Server ? 'https' : 'http';
+    return protocol + '://127.0.0.1:' + port + path;
+}
+
+function buildTestClient() {
+    const server = http.createServer(newtritionApp);
+    const address = getAddress(server, '/api');
+
+    return new NewtritionClient(address, {
+        validateStatus: () => true
+    });
+}
+
+module.exports = {
+    buildTestClient
+}
