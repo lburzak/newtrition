d5eb09bf77e8580111b5c2e1c68df715af7f7c5b
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2022-01-20 17:19:13 +0100

    Fix default branch warning

--- packages/web-app/src/util/color.js
+++ packages/web-app/src/util/color.js
@@ -18,6 +18,7 @@ export function rainbow(numOfSteps, step) {
         case 3: r = 0; g = q; b = 1; break;
         case 4: r = f; g = 0; b = 1; break;
         case 5: r = 1; g = 0; b = q; break;
+        default: r = 0; g = 0; b = 0;
     }
     const c = ("00" + (~~(r * 255)).toString(16)).slice(-2) + ("00" + (~~(g * 255)).toString(16)).slice(-2) + ("00" + (~~(b * 255)).toString(16)).slice(-2);
     return (c);
