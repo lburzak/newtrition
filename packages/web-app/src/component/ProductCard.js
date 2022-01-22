9f3c839fcb0ae64bfc61dc12d44076864f3b4a6a
Łukasz Burzak <lukasz.burzak@pollub.edu.pl>
2022-01-22 20:02:03 +0100

    Fix fats not being displayed on product card

--- packages/web-app/src/component/ProductCard.js
+++ packages/web-app/src/component/ProductCard.js
@@ -2,7 +2,7 @@ import {getDefaultProductPhoto, randomPlaceholderGenerator} from "../util/photo"
 import {CardItem, Spec} from "./CardItem";
 
 export default function ProductCard({product, menu, generatePhoto = randomPlaceholderGenerator()}) {
-    const {calories, proteins, carbohydrates} = product.nutritionFacts
+    const {calories, protein, fat, carbohydrate} = product.nutritionFacts
     return <CardItem
         name={product.name}
         ean={product.ean}
@@ -10,7 +10,8 @@ export default function ProductCard({product, menu, generatePhoto = randomPlaceh
         imageSrc={product.photosCount > 0 ? getDefaultProductPhoto(product._id) : generatePhoto(product.name)}
         menu={menu}>
         <Spec name={'Calories'} value={calories}/>
-        <Spec name={'Proteins'} value={proteins}/>
-        <Spec name={'Carbs'} value={carbohydrates}/>
+        <Spec name={'Proteins'} value={protein}/>
+        <Spec name={'Fat'} value={fat}/>
+        <Spec name={'Carbs'} value={carbohydrate}/>
     </CardItem>
 }
